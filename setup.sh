c0103d3be051a73b681175d7d49238c94f25741a
Georgi Popov <georgi.popov@skyscanner.net>
2015-11-25 17:57:26 +0000

    setup script for ubuntu

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -e
+
+sudo apt-get install -y vim tmux
+ln -s ${PWD}/git/gitconfig ${HOME}/.gitconfig
+ln -s ${PWD}/vim/vimrc ${HOME}/.vimrc
+
+sudo ./emacs/install_emacs.sh
+mkdir -p ${HOME}/.emacs.d/
+ln -s ${PWD}/emacs/init.el ${HOME}/.emacs.d/init.el
+
+cat >> ${HOME}/.bashrc <<EOF
+
+alias g='git'
+
+export EDITOR=vim
+git_shell_config=${PWD}/git/git_shell_config
+if [ -f \$git_shell_config ]; then
+    . \$git_shell_config
+fi
+EOF
